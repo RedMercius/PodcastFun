7eee278d4d6cff0ceb35d9faf258aa48ebee21f9
MOTAH <Support@JohnnieRuffin.com>
2015-11-17 13:12:30 -0500

    added ability to check for External Storage

--- app/src/main/java/com/example/johnnie/podcastfun/DownloadControl.java
+++ app/src/main/java/com/example/johnnie/podcastfun/DownloadControl.java
@@ -53,6 +53,27 @@ public class DownloadControl extends Activity {
     // TODO: check to see if we can detect an SD card
     // TODO: check to see if there is enough space to download a file.
 
+    public boolean isExternalStorage(){
+        boolean mExternalStorageAvailable = false;
+        boolean mExternalStorageWriteable = false;
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            // We can read and write the media
+            mExternalStorageAvailable = true;
+            mExternalStorageWriteable = true;
+        } else if (Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            // We can only read the media
+            mExternalStorageAvailable = true;
+            mExternalStorageWriteable = false;
+        } else {
+            // Something else is wrong. It may be one of many other states, but all we need
+            //  to know is we can neither read nor write
+            mExternalStorageAvailable = mExternalStorageWriteable = false;
+        }
+        return mExternalStorageAvailable;
+    }
+
     /** Called when the activity is first created. */
     public DownloadControl(Activity context) {
         webPath = "http://www.JohnnieRuffin.com/audio/";
