97fa369ceeb9c046afb426122c4364747d6d573f
RedMercius <me@JohnnieRuffin.com>
2015-12-17 22:06:58 -0500

    reorganized licensing

--- app/src/main/java/com/RuffinApps/johnnie/oldtimeradio/MainActivity.java
+++ app/src/main/java/com/RuffinApps/johnnie/oldtimeradio/MainActivity.java
@@ -37,6 +37,7 @@ public class MainActivity extends AppCompatActivity {
     Button comedyButton;
     Button scifiButton;
     Button thrillerButton;
+
     private static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAgo/IdsqOpiQUVftNMPUJeHQ1JiaNKPD7b1ygx5Lp2XNpfv2NIqKWDftZFW7721kYKdOrG3YJFl1RK/pPQRJstH30OMEnTSSX+1VT3nauMX36GYuJFj4n7HKZcVZ5rndJaDIiBK8qO7xPNaGlJVMMWjnYLlzDpvGCAeNGD+vWc8NAvBrGaE6gWD0/rZByTjjx3RIxp6ZR9jHJEq5zS4ZN019rGQM5WyAxPTz/CL1G6Migojp0pWl3xuzBJgKg5Q0lMriq9JVMR15wL0MLd8c28+o7gy9OVz2e3arvjqXZWN8pV8+dzEStoMcuO07OFwTNoDA0VDvESi5Rp6xoM3LevQIDAQAB";
     private static final byte[] SALT = new byte[] { 99,77,75,29,18,57,73,63,72,32,01,54,54,04,69,29,38,28,56,06 };
 
@@ -53,38 +54,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         /*** Start License Checking ***/
-        mHandler = new Handler();
-        mLicenseCheckerCallback = new MyLicenseCheckerCallback();
-
-        TelephonyManager telephonyManager = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE);
-
-        String deviceId = telephonyManager.getDeviceId();
-        Log.e("Device Id", deviceId);
-
-        Toast.makeText(this, "Device_ID: " + deviceId,     Toast.LENGTH_SHORT).show();
-
-        mChecker = new LicenseChecker(getApplicationContext(), new ServerManagedPolicy(this, new AESObfuscator(SALT,
-                getPackageName(), deviceId)), BASE64_PUBLIC_KEY);
-
-
-        mChecker.checkAccess(mLicenseCheckerCallback);
-
-        // Init
+        didCheck = false;
         mpreferences = new SecurePreferences(this, "jotr-preferences", "Loki13026044", true);
+
         String user = mpreferences.getString("LicenseCheck");
 
         if (user != null) {
             if (user.equals("true")) {
-                didCheck = true;
+            didCheck = true;
             }
         }
 
         if (!didCheck) {
-            Toast.makeText(this, "Checking application license...",     Toast.LENGTH_SHORT).show();
+            mHandler = new Handler();
+            mLicenseCheckerCallback = new MyLicenseCheckerCallback();
+
+            String deviceId = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+            Log.e("Device Id", deviceId);
+
+            Toast.makeText(this, "Device_ID: " + deviceId, Toast.LENGTH_SHORT).show();
+
+            mChecker = new LicenseChecker(getApplicationContext(), new ServerManagedPolicy(this, new AESObfuscator(SALT,
+                    getPackageName(), deviceId)), BASE64_PUBLIC_KEY);
+
+
+            mChecker.checkAccess(mLicenseCheckerCallback);
+
+
+
+            Toast.makeText(this, "Checking application license...", Toast.LENGTH_SHORT).show();
             doCheck();
         }
-
         /*** End License Checking ***/
 
         comedyButton = (Button) findViewById(R.id.comedyButton);
