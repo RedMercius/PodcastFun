a3e4b23e5199acfcb08341ee71a771947db9873b
Johnnie Ruffin <Johnnie.Ruffin@Bayer.com>
2016-04-08 10:26:12 -0400

    Second check-in and additions

--- app/src/androidTest/java/com/RuffinApps/johnnie/oldtimeradiofree/ApplicationTest.java
+++ app/src/androidTest/java/com/RuffinApps/johnnie/oldtimeradiofree/ApplicationTest.java
@@ -5,7 +5,7 @@
  *
  */
 
-package com.RuffinApps.johnnie.oldtimeradio;
+package com.RuffinApps.johnnie.oldtimeradiofree;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
