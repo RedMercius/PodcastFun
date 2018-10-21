1c9390612729d1daf1d4e368bd7f50b4cd033e39
RedMercius <me@JohnnieRuffin.com>
2018-10-21 11:18:42 -0400

    Updated for OREO

---
+++ app/src/androidTest/java/com/example/johnnie/podcastfun/ApplicationTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2015 © Johnnie Ruffin
+ *
+ * Unless required by applicable law or agreed to in writing, software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ */
+
+package com.RuffinApps.johnnie.oldtimeradio;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
